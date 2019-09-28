f359befdd6a166fc6fd206a58fbb9fdc56bb1603
tiratom <backspace373@gmail.com>
2019-09-28 11:56:57 +0900

    削除確認画面の実装実施

--- src/main/resources/static/js/activity-edit.js
+++ src/main/resources/static/js/activity-edit.js
@@ -3,9 +3,9 @@
  */
 
 
-// TODO 削除して良いかの確認ウィンドウ
-function confirm(activityId){
+// 削除して良いかの確認ウィンドウ
+function deleteConfirm(){
 	if(window.confirm("この暇つぶしを削除してよろしいですか？")){
-		return;
+		document.deleteForm.submit();
 	}
 }
